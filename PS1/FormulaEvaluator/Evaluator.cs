99d2681052585deaf0ac66b1bd3b5bf6fbd0692b
Jeremy Whipple <whipplekidz1@msn.com>
2016-09-06 11:23:18 -0600

    Began project, and created the try/catch block around the token line

---
+++ PS1/FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FormulaEvaluator
+{
+    public static class Evaluator
+    {
+        public delegate int Lookup(String v);
+
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+            try
+            {
+                string[] substrings = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Invalid Character(s) in the equation");
+            }
+                // TODO...
+        }
+    }
+}
--- PS1/FormulaEvaluator/Properties/AssemblyInfo.cs
+++ PS1/FormulaEvaluator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestProject")]
+[assembly: AssemblyTitle("FormulaEvaluator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestProject")]
+[assembly: AssemblyProduct("FormulaEvaluator")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("758585a4-f7c7-4106-8e8a-b4d66ff2176e")]
+[assembly: Guid("c120feb8-424a-4292-949b-74ba3254c03d")]
 
 // Version information for an assembly consists of the following four values:
 //
